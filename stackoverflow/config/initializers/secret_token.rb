34ac080e2711c5a48e6b85d6cc57b8ca93786460
dbc gogo <dbc@dbc.com>
2013-04-24 09:55:36 -0700

    new rails project skeleton created

---
+++ stackoverflow/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stackoverflow::Application.config.secret_token = 'fd4b599c5204afe910ba0fa15949279af90aa314fd008170aeb78ff47b32ea73edb21a023826386087d9e270b077d1529376933bce015d5d1142728209df47a8'
